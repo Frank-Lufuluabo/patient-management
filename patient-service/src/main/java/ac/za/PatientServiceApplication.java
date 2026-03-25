d80b640b6430b33f84e621183e29d04e4eea1860
frankkalengay@gmail.com <frankkalengay@gmail.com>
2026-03-25 23:26:35 +0200

    feat: add patient-service

---
+++ patient-service/src/main/java/ac/za/PatientServiceApplication.java
@@ -0,0 +1,13 @@
+package ac.za;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientServiceApplication.class, args);
+    }
+
+}
