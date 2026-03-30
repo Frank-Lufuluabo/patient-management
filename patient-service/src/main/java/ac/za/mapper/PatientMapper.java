c84a838a18995486c04fad230a7b3baf77589c33
frankkalengay@gmail.com <frankkalengay@gmail.com>
2026-03-30 23:57:15 +0200

    refactor: update PatientMapper implementation

--- patient-service/src/main/java/ac/za/mapper/PatientMapper.java
+++ patient-service/src/main/java/ac/za/mapper/PatientMapper.java
@@ -1,7 +1,10 @@
 package ac.za.mapper;
 
+import ac.za.dto.PatientRequestDTO;
 import ac.za.dto.PatientResponseDTO;
 import ac.za.model.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
     public static PatientResponseDTO toDTO(Patient patient) {
@@ -13,5 +16,15 @@ public class PatientMapper {
         patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
 
         return patientDTO;
+    }
+
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return patient;
     }
 }
