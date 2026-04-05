9eee7dfeebc2023870bc97bd67ff979f5f90af8f
frankkalengay@gmail.com <frankkalengay@gmail.com>
2026-04-05 23:59:25 +0200

    feat: add Kafka class

---
+++ patient-service/src/main/java/ac/za/kafka/KafkaProducer.java
@@ -0,0 +1,47 @@
+package ac.za.kafka;
+
+import ac.za.model.Patient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Service
+public class KafkaProducer {
+    private static final Logger log = LoggerFactory.getLogger(
+            KafkaProducer.class);
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendPatientCreatedEvent(Patient patient) {
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .build();
+
+        try {
+            kafkaTemplate.send("patient.created", event.toByteArray());
+        } catch (Exception e) {
+            log.error("Error sending PatientCreated event: {}", event);
+        }
+    }
+
+    public void sendPatientUpdatedEvent(Patient patient) {
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .build();
+
+        try {
+            kafkaTemplate.send("patient.updated", event.toByteArray());
+        } catch (Exception e) {
+            log.error("Error sending PatientUpdated event: {}", event);
+        }
+    }
+}
