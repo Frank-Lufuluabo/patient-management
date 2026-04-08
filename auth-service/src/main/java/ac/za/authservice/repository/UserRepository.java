a1585894d6b51d4a9f4b305a6a795e9bb3c9c911
frankkalengay@gmail.com <frankkalengay@gmail.com>
2026-04-08 23:36:06 +0200

    feat: add repository layer to auth-service

---
+++ auth-service/src/main/java/ac/za/authservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package ac.za.authservice.repository;
+
+import ac.za.authservice.model.User;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+  Optional<User> findByEmail(String email);
+}
