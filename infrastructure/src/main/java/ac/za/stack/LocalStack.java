86634578817ad086f6b691c2277f3405c7694fb8
frankkalengay@gmail.com <frankkalengay@gmail.com>
2026-05-14 23:35:40 +0200

    feat: Add Cluster

--- infrastructure/src/main/java/ac/za/stack/LocalStack.java
+++ infrastructure/src/main/java/ac/za/stack/LocalStack.java
@@ -1,11 +1,16 @@
 package ac.za.stack;
 
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.ISubnet;
 import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.services.ecs.CloudMapNamespaceOptions;
 import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.msk.CfnCluster;
+
+import java.util.stream.Collectors;
 
 public class LocalStack extends Stack {
+    private final Vpc vpc;
     public LocalStack(final App scope, final String id, final StackProps props) {
         super(scope, id, props);
     }
@@ -15,6 +20,21 @@ public class LocalStack extends Stack {
                 .create(this, "PatientManagementVPC")
                 .vpcName("PatientManagementVPC")
                 .maxAzs(2)
+                .build();
+    }
+
+    private CfnCluster createMskCluster(){
+        return CfnCluster.Builder.create(this, "MskCluster")
+                .clusterName("kafa-cluster")
+                .kafkaVersion("2.8.0")
+                .numberOfBrokerNodes(2)
+                .brokerNodeGroupInfo(CfnCluster.BrokerNodeGroupInfoProperty.builder()
+                        .instanceType("kafka.m5.xlarge")
+                        .clientSubnets(vpc.getPrivateSubnets().stream()
+                                .map(ISubnet::getSubnetId)
+                                .collect(Collectors.toList()))
+                        .brokerAzDistribution("DEFAULT")
+                        .build())
                 .build();
     }
 
